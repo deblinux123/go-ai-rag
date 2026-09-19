f8f01353a411cc109486c1e46b3a75c50cc13637
farid <deltaburs@gmail.com>
2026-09-19 09:44:42 +0330

    insertin and see the rows or get the one result

--- day-11/first/main.go
+++ day-11/first/main.go
@@ -23,4 +23,72 @@ func main() {
 	}
 
 	fmt.Println("Database connection successful!")
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS chats (
+			id 	INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL
+		)
+	`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Table created.")
+
+	result, err := db.Exec(
+		"INSERT INTO chats (title) VALUES (?)",
+		"this is my first chat that save to database",
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Chat created with id: ", id)
+
+	rows, err := db.Query("SELECT id, title FROM chats 	WHERE id > 1")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var title string
+
+		err := rows.Scan(&id, &title)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%d - %s\n", id, title)
+	}
+
+	var newId int
+	var title string
+
+	err = db.QueryRow(
+		"SELECT id, title FROM chats WHERE id=?",
+		3,
+	).Scan(&newId, &title)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("")
+	fmt.Println("Found Chat:")
+	fmt.Printf("%d : %s\n", newId, title)
+
 }
